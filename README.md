06d6ec26987dc232b25e7953e5a5bb5dd640e05c
Kamil Wróblewski <k.wroblewski54@gmail.com>
2025-04-07 07:40:27 +0200

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# SnakeRay
+A simple retro-style, snake game created for educational purposes, made in C++ and Raylib library.
---
+++ premake5.lua
@@ -0,0 +1,85 @@
+workspace "SnakeRay"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+filter "system:windows"
+    defines { "ClosedWindow=Raylib_CloseWindow"}
+filter {}
+
+project "raylib"
+    kind "StaticLib"
+    language "C++"
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir)
+
+    files{
+        "3rdParty/raylib/src/*.c",
+        "3rdParty/raylib/src/*h"
+    }
+
+    includedirs{
+        "3rdParty/raylib/src",
+        "3rdParty/raylib/src/external/glfw/include"
+    }
+
+    filter "system:windows"
+        defines { "PLATFORM_WINDOWS", "PLATFORM_DESKTOP" }
+        links { "winmm", "opengl32", "gdi32" }
+    filter {}
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        defines { "DEBUG" }
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+        defines { "NDEBUG" }
+
+    filter {}
+
+project "SnakeRay"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    targetdir ("bin/" .. outputdir)
+    objdir ("bin-int/" .. outputdir)
+
+    files {
+        "src/**.cpp",
+        "src/**.h"
+    }
+
+    includedirs {
+        "3rdParty/raylib/src",
+        "3rdParty/raylib/src/external/glfw/include",
+        "src"
+    }
+
+    links {
+        "raylib"
+    }
+
+    filter "system:Windows"
+        defines { "PLATFORM_WINDOWS" }
+        links { "winmm", "opengl32", "gdi32" }
+
+    filter {}
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        defines { "DEBUG" }
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+        defines { "NDEBUG" }
+    filter{}
